14a744887e45ef225a153c2db30f87a4350ac221
Elida <blubber3210@gmail.com>
2025-12-17 16:11:53 +0100

    fin

---
+++ CookBook/Controller.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookBook
+{
+    internal class Controller
+    {
+        public static void Run(List<Recipe> recipes)
+        {
+            //foreach (var recipe in recipes)
+            //{
+            //    recipe.PrintRecipeNames();
+            //}
+            while (true)
+            {
+                PrintIntroMenu();
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1) Recipe.PrintRecipeNames(recipes);
+                if (input == 2)
+                {
+                    var filteredList = PrintCategoryMenu(recipes);
+                    Recipe.PrintRecipeNames(filteredList);
+                }
+
+                if (input == 3)
+                {
+                    var filteredList = PrintIngredientMenu(recipes);
+                    Recipe.PrintRecipeNames(filteredList);
+                }
+                else Run(recipes);
+            }
+        }
+
+        private static void PrintIntroMenu()
+        {
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. View all recipes");
+            Console.WriteLine("2. Search by category");
+            Console.WriteLine("3. Search by ingredients");
+        }
+
+        private static List<Recipe> PrintCategoryMenu(List<Recipe> recipes)
+        {
+            Console.Clear();
+            var categories = new List<string> { "Breakfast", "Lunch", "Dinner", "Dessert", "Vegetarian", "Vegan", "Quick", "Comfort Food", "Italian", "Mexican", "Asian", "Healthy" };
+            Console.WriteLine("What kind of food do you want to make?");
+            for (int i = 0; i < categories.Count; i++)
+            {
+                Console.WriteLine(i + ": " + categories[i]);
+            }
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            var selectedCategory = categories[input];
+            List<Recipe> filteredList = recipes.Where(recipe => recipe.Categories.Contains(selectedCategory)).ToList();
+            return filteredList;
+        }
+
+        private static List<Recipe> PrintIngredientMenu(List<Recipe> recipes)
+        {
+            Console.Clear();
+            var ingredients = new List<string> { "Eggs", "Chicken", "Beef", "Pasta", "Rice", "Tomatoes", "Cheese", "Beans", "Potatoes", "Spinach" };
+            Console.WriteLine("What do you want to cook with?");
+
+            for (int i = 0; i < ingredients.Count; i++)
+            {
+                Console.WriteLine(i + ": " + ingredients[i]);
+            }
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            var selectedIngredient = ingredients[input];
+            
+            List<Recipe> filteredList = recipes.Where(recipe => recipe.Ingredients.Contains(selectedIngredient)).ToList();
+            return filteredList;
+        }
+    }
+}
---
+++ CookBook/Recipe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookBook
+{
+    internal class Recipe
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public List<string> Ingredients { get; private set; }
+        public List<string> Categories { get; private set; }
+
+        public Recipe(string name, string description, List<string> ingredients, List<string> categories)
+        {
+            Name = name;
+            Description = description;
+            Ingredients = ingredients;
+            Categories = categories;
+        }
+
+
+        public static void PrintRecipeNames(List<Recipe> recipes)
+        {
+            Console.Clear();
+            for (int i = 0; i < recipes.Count; i++)
+            {
+                Console.WriteLine(i + ": " + recipes[i].Name);
+            }
+            Console.WriteLine("Type index num to view");
+            int input = Convert.ToInt32(Console.ReadLine());
+            recipes[input].PrintRecipeInfo();
+
+        }
+
+        private void PrintRecipeInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("Recipe: " + Name);
+            Console.WriteLine("Description: " + Description);
+            Console.WriteLine("Ingredients needed: ");
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine("    " + ingredient);
+            }
+        }
+
+
+    }
+}
